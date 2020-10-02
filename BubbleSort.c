eb54e90569d9678b402b6a1d07cdbde4a0590b36
Madhura Pharande <42023529+Mmddzz3@users.noreply.github.com>
2020-10-03 00:45:14 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -5,3 +5,12 @@ void swap(int *xp, int *yp)
     *xp = *yp; 
     *yp = temp; 
 } 
+int main() 
+{ 
+    int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    bubbleSort(arr, n); 
+    printf("Sorted array: \n"); 
+    printArray(arr, n); 
+    return 0; 
+} 
