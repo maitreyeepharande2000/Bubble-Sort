9cf7d6935e160fcf297f47ca798d9eaf7f2ae7c5
Madhura Pharande <madhura.pharande@somaiya.edu>
2021-10-02 23:27:47 +0530

    Update bubblesort.java
    HacktoberFest PR 3

--- bubblesort.java
+++ bubblesort.java
@@ -15,4 +15,13 @@ class Main {
           array[j + 1] = temp;
         }
   }
+  public static void main(String args[]) {
+      
+    int[] data = { -2, 45, 0, 11, -9 };
+    
+    Main.bubbleSort(data);
+    
+    System.out.println("Sorted Array in Ascending Order:");
+    System.out.println(Arrays.toString(data));
+  }
 }
